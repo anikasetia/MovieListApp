6a7fc1680031e1c95ecff195a536b8e24f8545f6
Anika Setia <anika.setia18@gmail.com>
2022-08-05 17:14:23 +0530

    initial list UI with table view

--- MovieListApp/ViewController.swift
+++ MovieListApp/ViewController.swift
@@ -7,36 +7,71 @@
 
 import UIKit
 
-class ViewController: UIViewController, MovieDataManagerDelegate {
+class ViewController: UIViewController, MovieDataManagerDelegate, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
     
-    func didFailWithError(error: Error) {
-        print("API call failed \(error)")
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
     }
     
-    func didGetMovieListData(_ movieDataManager: MovieDataManager, movies: [MovieListModel]) {
-//        print("movies received")
-//        DispatchQueue.main.async {
-//            print(movies)
-//        }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "movieCell", for: indexPath)
+        cell.textLabel!.text = movies[indexPath.row].title
+                return cell
     }
     
-//    func didGetMovieDetail(_ movieDataManager: MovieDataManager, movieDetail: MovieModel) {
-//        print("movie detail received")
-//        print(movieDetail)
-//    }
+    
+    @IBOutlet weak var searchField: UITextField!
+    @IBOutlet weak var movieListTableView: UITableView!
+    @IBOutlet weak var movieListCell: UITableViewCell!
+    
+    var movies:[MovieListModel] = []
+    
+    var movieManager: MovieDataManager = MovieDataManager()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        searchField.delegate = self
+        movieListTableView.delegate = self
+        movieListTableView.dataSource = self
+        
+        searchField.addTarget(self, action: #selector(ViewController.textFieldDidChange(_:)),
+                                  for: .editingChanged)
+        
+        movieManager.delegate = self
+//        print(a.fetchMovieList(searchTerm: "sha", page: 1))
+//        print(a.performDetailApiRequest(titleId: "tt14713814"))
+    }
     
     func didGetMovieDetail(_ movieDataManager: MovieDataManager, movieDetail: MovieListModel) {
         print("movie detail received")
         print(movieDetail)
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        var a = MovieDataManager()
-        a.delegate = self
-        print(a.fetchMovieList(searchTerm: "sha", page: 1))
-        print(a.performDetailApiRequest(titleId: "tt14713814"))
+    
+    func didFailWithError(error: Error) {
+        print("API call failed \(error)")
+    }
+    
+    func didGetMovieListData(_ movieDataManager: MovieDataManager, movies: [MovieListModel]) {
+        print("movies received")
+        print(movies)
+        self.movies = movies
+        
+        
+        DispatchQueue.main.async {
+            print(movies)
+            self.movieListTableView.reloadData()
+        }
+    }
+    
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        print(textField.text!)
+        if(textField.text!.count >= 3){
+            print(movieManager.fetchMovieList(searchTerm: textField.text!, page: 1))
+            
+        }
     }
 
 
